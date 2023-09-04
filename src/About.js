3bc2f596cb8fe16309b2adf70bdfb80aeca4158f
sharuk0411 <shar19409.ec@rmkec.ac.in>
2023-09-04 17:41:00 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+        <h2>About</h2>
+        <p style={{marginTop:'1rem'}}>Hi, I am Sharuk. Togetherness is our goal, we try to connect this world through our Social Media application. For further queries contact smapp@gmail.com</p>
+    </main>
+  )
+}
+
+export default About
